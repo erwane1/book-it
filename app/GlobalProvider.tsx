9b8dd47fc12554567b0127439b02f31e68532755
andre <cocan.dev.ndiamoi.andre@gmail.com>
2023-11-23 10:59:43 +0000

    react-hos-toast added

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,3 +1,9 @@
+import { Toaster } from "react-hot-toast";
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
 }
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,3 +1,4 @@
+import RoomItem from "./room/RoomItem";
 const Home = () => {
   return (
     <div>
@@ -7,35 +8,7 @@ const Home = () => {
           <i className="fa fa-arrow-left"></i> Back to Search
         </a>
         <div className="row mt-4">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3 d-flex">
-            <div className="card p-2 w-100">
-              <img
-                className="card-img-top mx-auto"
-                src="images/default_room_image.jpg"
-                alt=""
-                height="170"
-                width="100"
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                  <a href="/rooms/roomId">Room Name</a>
-                </h5>
-                <div className="mt-auto">
-                  <p className="card-text mt-2">
-                    <b>$100</b> / night
-                  </p>
-                </div>
-                <div>
-                  <div>
-                    <span className="no-of-reviews">(50 Reviews)</span>
-                  </div>
-                  <a className="btn view-btn mt-3 w-100" href="/rooms/roomId">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <RoomItem />
         </div>
       </section>
     </div>
